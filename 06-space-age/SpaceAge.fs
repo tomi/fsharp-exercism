83142200be1313f6e1254acd28ee367033765b5d
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-02-18 21:50:52 +0200

    Add solution to space age

---
+++ 06-space-age/SpaceAge.fs
@@ -0,0 +1,27 @@
+﻿module SpaceAge
+
+type Planet =
+  | Earth
+  | Mercury
+  | Venus
+  | Mars
+  | Jupiter
+  | Saturn
+  | Uranus
+  | Neptune
+
+let earthOrbitalPeriod = 31557600.0
+
+let age (planet: Planet) (seconds: int64): float =
+  let orbitalPeriodInS =
+    match planet with
+    | Earth -> earthOrbitalPeriod
+    | Mercury -> 0.2408467 * earthOrbitalPeriod
+    | Venus -> 0.61519726 * earthOrbitalPeriod
+    | Mars -> 1.8808158 * earthOrbitalPeriod
+    | Jupiter -> 11.862615 * earthOrbitalPeriod
+    | Saturn -> 29.447498 * earthOrbitalPeriod
+    | Uranus -> 84.016846 * earthOrbitalPeriod
+    | Neptune -> 164.79132 * earthOrbitalPeriod
+
+  float seconds / orbitalPeriodInS
---
+++ 06-space-age/SpaceAgeTest.fs
@@ -0,0 +1,41 @@
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+
+module SpaceAgeTest
+
+open FsUnit.Xunit
+open Xunit
+
+open SpaceAge
+
+[<Fact>]
+let ``Age on Earth`` () =
+    age Earth 1000000000L |> should (equalWithin 0.01) 31.69
+
+[<Fact>]
+let ``Age on Mercury`` () =
+    age Mercury 2134835688L |> should (equalWithin 0.01) 280.88
+
+[<Fact>]
+let ``Age on Venus`` () =
+    age Venus 189839836L |> should (equalWithin 0.01) 9.78
+
+[<Fact>]
+let ``Age on Mars`` () =
+    age Mars 2329871239L |> should (equalWithin 0.01) 39.25
+
+[<Fact>]
+let ``Age on Jupiter`` () =
+    age Jupiter 901876382L |> should (equalWithin 0.01) 2.41
+
+[<Fact>]
+let ``Age on Saturn`` () =
+    age Saturn 3000000000L |> should (equalWithin 0.01) 3.23
+
+[<Fact>]
+let ``Age on Uranus`` () =
+    age Uranus 3210123456L |> should (equalWithin 0.01) 1.21
+
+[<Fact>]
+let ``Age on Neptune`` () =
+    age Neptune 8210123456L |> should (equalWithin 0.01) 1.58
+
