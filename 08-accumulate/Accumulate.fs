b9938a2e851d166fed01ab3440df341b2c1c8739
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-02-24 18:40:22 +0200

    Add solution to accumulate

---
+++ 08-accumulate/Accumulate.fs
@@ -0,0 +1,10 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+  let rec _accumulate xs acc =
+    match xs with
+    | [] -> acc
+    | head :: tail -> _accumulate tail (func head:: acc)
+
+  _accumulate input []
+  |> List.rev
