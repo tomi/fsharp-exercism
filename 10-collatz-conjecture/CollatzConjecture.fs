aaf68d9c2c0228f0f7ce1f2021573611092af365
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-02-24 19:25:39 +0200

    Add solution to collatz conjecture

---
+++ 10-collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,14 @@
+﻿module CollatzConjecture
+
+let (|Even|Odd|) number =
+  if number % 2 = 0 then Even else Odd
+
+let steps (number: int): int option =
+  let rec takeStep number numSteps =
+    match number with
+    | n when n <= 0 -> None
+    | 1 -> Some numSteps
+    | Even -> takeStep (number / 2) (numSteps + 1)
+    | Odd -> takeStep (3 * number + 1) (numSteps + 1)
+
+  takeStep number 0
