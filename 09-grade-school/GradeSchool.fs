e4c490d48d8b2d5f6ca18281d9483ede4281c439
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-02-24 19:15:09 +0200

    Add solution to grade school

---
+++ 09-grade-school/GradeSchool.fs
@@ -0,0 +1,20 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+  match Map.tryFind grade school with
+  | Some x -> Map.add grade (student::x) school
+  | None -> Map.add grade [student] school
+
+let roster (school: School): string list =
+  school
+  |> Map.toList
+  |> List.collect (fun (_, studentsOnGrade) -> List.sort studentsOnGrade)
+
+let grade (number: int) (school: School): string list =
+  match Map.tryFind number school with
+  | Some studentsOnGrade -> List.sort studentsOnGrade
+  | None -> []
