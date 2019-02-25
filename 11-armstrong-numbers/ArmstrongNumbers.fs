d2d56d97da4277136063f827868b3464f8780567
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-02-25 21:04:17 +0200

    Add solution to armstrong numbers

---
+++ 11-armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,9 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool =
+  let numDigits = number.ToString().Length
+
+  number.ToString()
+    |> Seq.map (string >> int)
+    |> Seq.sumBy (fun x -> pown x numDigits)
+    |> (=) number
