f6cf22d7ffb729913995f6f4dc5822bc93766424
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-03-09 14:39:18 +0200

    Add solution to robot simulator

---
+++ 16-robot-simulator/RobotSimulator.fs
@@ -0,0 +1,42 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { direction: Direction; position: Position }
+
+let create direction position =
+  {
+    direction = direction
+    position = position
+  }
+
+let rotateRight { direction=dir; position=pos } =
+  match dir with
+  | North -> create East pos
+  | East -> create South pos
+  | South -> create West pos
+  | West -> create North pos
+
+let rotateLeft { direction=dir; position=pos } =
+  match dir with
+  | North -> create West pos
+  | East -> create North pos
+  | South -> create East pos
+  | West -> create South pos
+
+let advance { direction=dir; position=(x,y) } =
+  match dir with
+  | North -> create dir (x, y + 1)
+  | East -> create dir (x + 1, y)
+  | South -> create dir (x, y - 1)
+  | West -> create dir (x - 1, y)
+
+let handleInstruction robot instruction =
+  match instruction with
+  | 'R' -> rotateRight robot
+  | 'L' -> rotateLeft robot
+  | 'A' -> advance robot
+  | _ -> failwith "Unknown command"
+
+let move instructions robot =
+  instructions |> Seq.fold handleInstruction robot
---
+++ 16-robot-simulator/RobotSimulatorTest.fs
@@ -0,0 +1,115 @@
+// This file was auto-generated based on version 3.1.0 of the canonical data.
+
+module RobotSimulatorTest
+
+open FsUnit.Xunit
+open Xunit
+
+open RobotSimulator
+
+[<Fact>]
+let ``Robots are created with a position and direction`` () =
+    let expected = create Direction.North (0, 0)
+    create Direction.North (0, 0) |> should equal expected
+
+[<Fact>]
+let ``Negative positions are allowed`` () =
+    let expected = create Direction.South (-1, -1)
+    create Direction.South (-1, -1) |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from north to east`` () =
+    let robot = create Direction.North (0, 0)
+    let expected = create Direction.East (0, 0)
+    move "R" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from east to south`` () =
+    let robot = create Direction.East (0, 0)
+    let expected = create Direction.South (0, 0)
+    move "R" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from south to west`` () =
+    let robot = create Direction.South (0, 0)
+    let expected = create Direction.West (0, 0)
+    move "R" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from west to north`` () =
+    let robot = create Direction.West (0, 0)
+    let expected = create Direction.North (0, 0)
+    move "R" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from north to west`` () =
+    let robot = create Direction.North (0, 0)
+    let expected = create Direction.West (0, 0)
+    move "L" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from west to south`` () =
+    let robot = create Direction.West (0, 0)
+    let expected = create Direction.South (0, 0)
+    move "L" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from south to east`` () =
+    let robot = create Direction.South (0, 0)
+    let expected = create Direction.East (0, 0)
+    move "L" robot |> should equal expected
+
+[<Fact>]
+let ``Changes the direction from east to north`` () =
+    let robot = create Direction.East (0, 0)
+    let expected = create Direction.North (0, 0)
+    move "L" robot |> should equal expected
+
+[<Fact>]
+let ``Increases the y coordinate one when facing north`` () =
+    let robot = create Direction.North (0, 0)
+    let expected = create Direction.North (0, 1)
+    move "A" robot |> should equal expected
+
+[<Fact>]
+let ``Decreases the y coordinate by one when facing south`` () =
+    let robot = create Direction.South (0, 0)
+    let expected = create Direction.South (0, -1)
+    move "A" robot |> should equal expected
+
+[<Fact>]
+let ``Increases the x coordinate by one when facing east`` () =
+    let robot = create Direction.East (0, 0)
+    let expected = create Direction.East (1, 0)
+    move "A" robot |> should equal expected
+
+[<Fact>]
+let ``Decreases the x coordinate by one when facing west`` () =
+    let robot = create Direction.West (0, 0)
+    let expected = create Direction.West (-1, 0)
+    move "A" robot |> should equal expected
+
+[<Fact>]
+let ``Instructions to move east and north from README`` () =
+    let robot = create Direction.North (7, 3)
+    let expected = create Direction.West (9, 4)
+    move "RAALAL" robot |> should equal expected
+
+[<Fact>]
+let ``Instructions to move west and north`` () =
+    let robot = create Direction.North (0, 0)
+    let expected = create Direction.West (-4, 1)
+    move "LAAARALA" robot |> should equal expected
+
+[<Fact>]
+let ``Instructions to move west and south`` () =
+    let robot = create Direction.East (2, -7)
+    let expected = create Direction.South (-3, -8)
+    move "RRAAAAALA" robot |> should equal expected
+
+[<Fact>]
+let ``Instructions to move east and north`` () =
+    let robot = create Direction.South (8, 4)
+    let expected = create Direction.North (11, 5)
+    move "LAAARRRALLLL" robot |> should equal expected
+
